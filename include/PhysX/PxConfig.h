8c414563126ae508980be26e83d98ee00d83a543
Xie Hao Zhe <xiehaozhe@gmail.com>
2022-04-24 18:57:34 +0800

    physx 4.1 static lib

--- include/PhysX/PxConfig.h
+++ include/PhysX/PxConfig.h
@@ -23,13 +23,12 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Copyright (c) 2008-2019 NVIDIA Corporation. All rights reserved.
+// Copyright (c) 2008-2021 NVIDIA Corporation. All rights reserved.
 
 
 #ifndef PX_CONFIG
 #define PX_CONFIG
 
-#define PX_PHYSX_STATIC_LIB
 
 
 #endif  // PX_CONFIG
